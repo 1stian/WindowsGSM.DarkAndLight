62af88cd9f0e2900f794db81a1fcfed98e6a1a0d
Stian T <stian@ohnee.no>
2020-09-05 17:47:30 +0200

    Add files via upload

---
+++ DarkAndLight.cs/DarkAndLight.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using WindowsGSM.Functions;
+using WindowsGSM.GameServer.Query;
+using WindowsGSM.GameServer.Engine;
+using System.IO;
+
+namespace WindowsGSM.Plugins
+{
+    public class DarkAndLight : SteamCMDAgent
+    {
+        // - Plugin Details
+        public Plugin Plugin = new Plugin
+        {
+            name = "WindowsGSM.DarkAndLight", // WindowsGSM.XXXX
+            author = "1stian",
+            description = "WindowsGSM plugin that adds support for Dark & Light dedicated servers.",
+            version = "1.0",
+            url = "https://github.com/1stian/WindowsGSM.DarkAndLight", // Github repository link (Best practice)
+            color = "#c0883e" // Color Hex
+        };
+
+        // - Settings properties for SteamCMD installer
+        public override bool loginAnonymous => true;
+        public override string AppId => "630230"; // Game server appId, Dark & Light is 630230
+
+        // - Standard Constructor and properties
+        public DarkAndLight(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
+        private readonly ServerConfig _serverData;
+        public string Error, Notice;
+
+
+        // - Game server Fixed variables
+        public override string StartPath => @"DNL\Binaries\Win64\DNLServer.exe"; // Game server start path
+        public string FullName = "Dark & Light"; // Game server FullName
+        public bool AllowsEmbedConsole = false;  // Does this server support output redirect?
+        public int PortIncrements = 1; // This tells WindowsGSM how many ports should skip after installation
+        public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
+
+
+        // - Game server default values
+        public string Port = "7777"; // Default port
+        public string QueryPort = "27016"; // Default query port
+        public string Defaultmap = "DNL_ALL"; // Default map name
+        public string Maxplayers = "70"; // Default maxplayers
+        public string Additional = "ServerPassword=PASSWORD?ServerAdminPassword=MYADMINPASSWORD"; // Additional server start parameter
+
+
+        // - Create a default cfg for the game server after installation
+        public async void CreateServerCFG()
+        {
+            
+        }
+
+        // - Start server function, return its Process to WindowsGSM
+        public async Task<Process> Start()
+        {
+            // Check for files in Win64
+            string win64 = Path.Combine(ServerPath.GetServersServerFiles(_serverData.ServerID, @"DNL\Binaries\Win64\"));
+            string[] neededFiles = { "steamclient64.dll", "tier0_s64.dll", "vstdlib_s64.dll" };
+
+            foreach (string file in neededFiles)
+            {
+                if (!File.Exists(Path.Combine(win64, file)))
+                {
+                    File.Copy(Path.Combine(ServerPath.GetServersServerFiles(_serverData.ServerID), file), Path.Combine(win64, file));
+                }
+            }
+
+            string shipExePath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath);
+
+            // Prepare start parameter
+            string param = string.IsNullOrWhiteSpace(_serverData.ServerMap) ? string.Empty : $"{_serverData.ServerMap}?listen";
+            param += string.IsNullOrWhiteSpace(_serverData.ServerName) ? string.Empty : $"?SessionName={_serverData.ServerName}";
+            param += string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $"?MultiHome={_serverData.ServerIP}";
+            param += string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $"?Port={_serverData.ServerPort}";
+            param += string.IsNullOrWhiteSpace(_serverData.ServerQueryPort) ? string.Empty : $"?QueryPort={_serverData.ServerQueryPort}";
+            param += string.IsNullOrWhiteSpace(_serverData.ServerMaxPlayer) ? string.Empty : $"?MaxPlayers={_serverData.ServerMaxPlayer}";
+            param += $"?{_serverData.ServerParam} -server -log";
+
+            // Prepare Process
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
+                    FileName = shipExePath,
+                    Arguments = param,
+                    WindowStyle = ProcessWindowStyle.Minimized,
+                    UseShellExecute = false
+                },
+                EnableRaisingEvents = true
+            };
+
+            // Set up Redirect Input and Output to WindowsGSM Console if EmbedConsole is on
+            if (AllowsEmbedConsole)
+            {
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                var serverConsole = new ServerConsole(_serverData.ServerID);
+                p.OutputDataReceived += serverConsole.AddOutput;
+                p.ErrorDataReceived += serverConsole.AddOutput;
+
+                // Start Process
+                try
+                {
+                    p.Start();
+                }
+                catch (Exception e)
+                {
+                    Error = e.Message;
+                    return null; // return null if fail to start
+                }
+
+                p.BeginOutputReadLine();
+                p.BeginErrorReadLine();
+                return p;
+            }
+
+            // Start Process
+            try
+            {
+                p.Start();
+                return p;
+            }
+            catch (Exception e)
+            {
+                Error = e.Message;
+                return null; // return null if fail to start
+            }
+        }
+
+
+        // - Stop server function
+        public async Task Stop(Process p) => await Task.Run(() => { p.Kill(); });
+    }
+}
